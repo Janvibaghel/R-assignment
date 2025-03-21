44429ff454119604bf734eeb1f06247db197f3bf
janvi <166206890+Janvibaghel@users.noreply.github.com>
2025-03-21 17:06:39 +0530

    Delete Q11.R

--- Q11.R
+++
@@ -1,4 +0,0 @@
-# Replace first occurrence of "apple" using sub() and all occurrences using gsub()
-text <- "apple, apple, and apple"
-print(sub("apple", "oranGe", text))  # Replace first occurrence
-print(gsub("apple", "oranGe", text)) # Replace all occurrences
