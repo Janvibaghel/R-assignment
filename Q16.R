2cef1c37f39fcbfbfc4db1945579857b4e2691df
janvi <166206890+Janvibaghel@users.noreply.github.com>
2025-03-21 17:07:27 +0530

    Delete Q16.R

--- Q16.R
+++
@@ -1,4 +0,0 @@
-# Demonstrate any() and all() functions
-log_vec <- c(TRUE, FALSE, TRUE, FALSE)
-print(any(log_vec)) # TRUE if at least one TRUE
-print(all(log_vec)) # FALSE because not all are TRUE
