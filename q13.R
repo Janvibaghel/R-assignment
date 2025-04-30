3eb9036999b6f88944a03f7c80c5de5a4601788f
janvi <166206890+Janvibaghel@users.noreply.github.com>
2025-05-01 00:02:38 +0530

    Delete q13.R

--- q13.R
+++
@@ -1,9 +0,0 @@
-# 7. Create a simple data frame
-df <- data.frame(
-  Name = c("Alice", "Bob", "Charlie"),
-  Age = c(25, 30, 22)
-)
-
-# 13. Convert character column to factor
-df$Name <- as.factor(df$Name)
-
