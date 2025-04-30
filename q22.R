7e9e08eb2f33c41cdc2e8107e503e1a5db58f5cd
janvi <166206890+Janvibaghel@users.noreply.github.com>
2025-05-01 00:04:30 +0530

    Delete q22.R

--- q22.R
+++
@@ -1,7 +0,0 @@
-# 22. Create dataframe with recycling
-recycled_df <- data.frame(
-  A = 1:5,
-  B = c("x", "y")  # Will be recycled to match length of A
-)
-# This demonstrates R's recycling behavior where shorter vectors
-# are repeated to match the length of the longest vector
